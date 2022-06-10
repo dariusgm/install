8182f89e4f36feedaca5d626965f5a95700cbff1
Darius Murawski <darius@murawski.blog>
2022-06-10 21:02:59 +0200

    fix streamlit

--- ubuntu/streamdeck.sh
+++ ubuntu/streamdeck.sh
@@ -1,5 +1,6 @@
 sudo apt update && sudo apt upgrade
-sudo apt install libhidapi-libusb0 libxcb-xinerama0
+sudo apt install -y libhidapi-libusb0 libxcb-xinerama0
 sudo cp ../streamdeck/70-streamdeck.rules /etc/udev/rules.d/70-streamdeck.rules
 sudo udevadm control --reload-rules
-pip3 install --user streamdeck_ui
+pip3  install --user streamdeck_ui
+cp ../streamdeck/streamdeck_autostart.desktop ~/.config/autostart
