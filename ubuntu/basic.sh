5a43a409dd50a8c3df1228b3c2e3c90495e01711
Darius Murawski <darius@murawski.blog>
2022-09-07 21:51:28 +0200

    add jq to basic install

--- ubuntu/basic.sh
+++ ubuntu/basic.sh
@@ -2,5 +2,5 @@ sudo apt-get update -y
 # net-tools for ifconfig
 # nethog for network traffic monitoring
 # iotop for IO monitoring
-sudo apt-get install -y curl git vim ncdu net-tools nethogs htop iotop
+sudo apt-get install -y curl git vim ncdu net-tools nethogs htop iotop jq
 
