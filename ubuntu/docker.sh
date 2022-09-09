2ef955178976db9a50d8d11a01daed5ab9325a1e
Darius Murawski <dariusmurawski@hotmail.com>
2022-09-09 19:38:07 +0200

    fix sudo requirements for docker

--- ubuntu/docker.sh
+++ ubuntu/docker.sh
@@ -6,7 +6,13 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --yes --dearm
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update -y
 sudo apt install docker-ce docker-ce-cli containerd.io -y
-pip3 install wheel || true
-pip3 install docker-compose || true
+
+sudo groupadd -f docker
 sudo usermod -aG docker $USER || true
 newgrp docker
+
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
+
+pip3 install wheel || true
+pip3 install docker-compose || true
