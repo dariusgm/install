c1f4bc6f22d9c8a5ed60b52a93792218a192b97f
Darius Murawski <darius@murawski.blog>
2022-09-27 21:14:19 +0200

    add optional dependencies to kdenlive

--- ubuntu/kdenlive.sh
+++ ubuntu/kdenlive.sh
@@ -1,4 +1,5 @@
 sudo add-apt-repository -y ppa:kdenlive/kdenlive-stable
 sudo apt update -y
 # melt required for rendering
-sudo apt install -y kdenlive melt
+sudo apt install -y kdenlive melt mediainfo breeze-icon-theme frei0r-plugins libsdl-console libsdl-console-dev  libavcodec-extra
+sudo snap install glaxnimate audacity
