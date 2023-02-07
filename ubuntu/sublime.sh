489653bb8f7662cb16fe89742fd62b307bd154fc
Darius Murawski <dariusmurawski@hotmail.com>
2023-02-07 15:44:22 +0100

    Force use of sublime 3

--- ubuntu/sublime.sh
+++ ubuntu/sublime.sh
@@ -1,12 +1,18 @@
 # download manual latest
 sudo apt-get install -y libgtk-3-0 curl
-curl https://download.sublimetext.com/sublime-text_build-4126_amd64.deb -o sublime-text_build-4126_amd64.deb
-sudo dpkg -i sublime-text_build-4126_amd64.deb
-rm sublime-text_build-4126_amd64.deb
+curl https://download.sublimetext.com/sublime-text_build-3211_amd64.deb -o sublime-text_build-3211_amd64.deb
+sudo dpkg -i sublime-text_build-3211_amd64.deb
+rm sublime-text_build-3211_amd64.deb
 # sudo npm install -g diagnostic-languageserver
 
 # install snippets and settings
-mkdir -p ~/.config/sublime-text/Packages/User
 
-cp -f ~/install/*.sublime-settings ~/.config/sublime-text/Packages/User
-cp -f ~/install/*.sublime-snippet ~/.config/sublime-text/Packages/User
+# for sublime 4
+# mkdir -p ~/.config/sublime-text/Packages/User
+# cp -f ~/install/*.sublime-settings ~/.config/sublime-text/Packages/User
+# cp -f ~/install/*.sublime-snippet ~/.config/sublime-text/Packages/User
+
+# for sublime 3
+mkdir -p ~/.config/sublime-text-3/Packages/User
+cp -f ~/install/*.sublime-settings ~/.config/sublime-text-3/Packages/User
+cp -f ~/install/*.sublime-snippet ~/.config/sublime-text-3/Packages/User
