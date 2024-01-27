18be381ced2c564c59edfde51aecb4ace01f5c2b
Darius Murawski <darius@murawski.blog>
2024-01-27 17:28:30 +0100

    add dbeaver

--- ubuntu/snap.sh
+++ ubuntu/snap.sh
@@ -5,3 +5,4 @@ snap install --beta filezilla
 sudo snap install --edge --devmode calibre
 sudo snap install --edge filezilla
 cp ./spotify_autostart.desktop ~/.config/autostart
+sudo snap install dbeaver-ce
