799ac9b853c83bc98744603de2437f43c2ed8adb
Darius Murawski <darius@murawski.blog>
2024-05-20 21:31:28 +0200

    add graphviz for ubuntu

--- ubuntu/basic.sh
+++ ubuntu/basic.sh
@@ -2,4 +2,5 @@ sudo apt-get update -y
 # net-tools for ifconfig
 # nethog for network traffic monitoring
 # iotop for IO monitoring
-sudo apt-get install -y curl git vim ncdu net-tools nethogs htop iotop jq
+# graphviz for rendering plantuml
+sudo apt-get install -y curl git vim ncdu net-tools nethogs htop iotop jq graphviz
