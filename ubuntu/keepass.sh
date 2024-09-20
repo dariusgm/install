cf4e3a737a6b2c79505e2d6616c3d59a4a47a50c
Darius Murawski <dariusmurawski@hotmail.com>
2024-09-20 18:41:54 +0200

    Use Snap from Keepass Web

--- ubuntu/keepass.sh
+++ ubuntu/keepass.sh
@@ -4,5 +4,5 @@ git clone https://github.com/keeweb/keeweb
 cd keeweb
 git checkout $(git tag -l --sort -version:refname | head -n 1 ) 
 export LATEST=$(git branch | head -n 1 | awk -F'v' '{print substr($2, 0, length($2) -1) }')
-wget https://github.com/keeweb/keeweb/releases/download/v$LATEST/KeeWeb-$LATEST.linux.x64.deb
-sudo dpkg -i KeeWeb-$LATEST.linux.x64.deb
+curl -L https://github.com/keeweb/keeweb/releases/download/v$LATEST/KeeWeb-$LATEST.linux.snap -o KeeWeb-$LATEST.linux.snap
+sudo snap install --dangerous KeeWeb-$LATEST.linux.snap
