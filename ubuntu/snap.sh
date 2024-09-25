2c19ce0253b289731bc37b2923e8f8ee641a01af
Darius Murawski <dariusmurawski@hotmail.com>
2024-09-25 22:48:09 +0200

    Update snap packages

--- ubuntu/snap.sh
+++ ubuntu/snap.sh
@@ -1,9 +1,22 @@
+echo "Installing spotify"
 sudo snap install spotify
+cp ./spotify_autostart.desktop ~/.config/autostart
+
+echo "Installing gimp"
 sudo snap install gimp
+
+echo "Installing discord"
 sudo snap install discord
+
+echo "Installing filezilla"
 snap install --beta filezilla
 # sudo snap install --edge --devmode calibre
-sudo snap install --edge filezilla
-cp ./spotify_autostart.desktop ~/.config/autostart
+
+echo "Installing dbeaver-ce"
 sudo snap install dbeaver-ce
+
+echo "Installing brave"
 sudo snap install brave
+
+echo "Installing keepassxc"
+sudo snap install keepassxc
