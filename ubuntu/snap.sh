a53d4afc919a6f1fb01f7b3161e6f9d173633eee
Darius Murawski <dariusmurawski@hotmail.com>
2024-09-25 22:35:32 +0200

    Update snap.sh
    Disable Calibre

--- ubuntu/snap.sh
+++ ubuntu/snap.sh
@@ -2,7 +2,7 @@ sudo snap install spotify
 sudo snap install gimp
 sudo snap install discord
 snap install --beta filezilla
-sudo snap install --edge --devmode calibre
+# sudo snap install --edge --devmode calibre
 sudo snap install --edge filezilla
 cp ./spotify_autostart.desktop ~/.config/autostart
 sudo snap install dbeaver-ce
